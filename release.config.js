903a4dae76ce4d52724f89c474ede1369e54c801
chenjiahui <2675957434@qq.com>
2024-04-30 13:40:25 +0800

    ci(semantic release): change policy
    [skip ci]

--- release.config.js
+++ release.config.js
@@ -11,7 +11,7 @@ module.exports = {
     branches : [
         "master",
         "main",
-        '*/main',
+        { name: '*/main', prerelease: false},
         {name: 'test', prerelease: true},
         {name: '*/test', prerelease: true}
     ],
